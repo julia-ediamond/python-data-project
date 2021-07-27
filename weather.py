8942620d3d847e0ef1fe5ac9eb3bf236426ad2c7
iusharnina@gmail.com <zjulia679@gmail.com>
2021-07-27 16:05:50 +1000

    Add conversion of F to C temperature

--- weather.py
+++ weather.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+import statistics
 
 # with open("tests/data/example_one.csv") as csv_file:
 #    csv_reader = csv.reader(csv_file, delimiter=",")
@@ -36,21 +37,23 @@ def convert_date(iso_string):
         csv_reader = csv.reader(csv_file, delimiter=",")
         next(csv_reader)
         for iso_string in csv_reader:
-            #date = datetime.datetime(csv_reader)
-            #date = datetime(int(year), int(month), 1)
-            #date = date.strftime("%m/%d/%Y, %H:%M")
-            #date = datetime.datetime(2018, 9, 15)
+            # date = datetime.datetime(csv_reader)
+            # date = datetime(int(year), int(month), 1)
+            # date = date.strftime("%m/%d/%Y, %H:%M")
+            # date = datetime.datetime(2018, 9, 15)
 
-            #print(date.strftime("%b %d %Y %H:%M:%S"))
+            # print(date.strftime("%b %d %Y %H:%M:%S"))
             # print(f"{date}")
-            #date_object = datetime.strptime(date[0], '%m-%d-%y')
-            #date_object = [date[0].strftime('%m-%d-%Y') for date in csv_reader]
+            # date_object = datetime.strptime(date[0], '%m-%d-%y')
+            # date_object = [date[0].strftime('%m-%d-%Y') for date in csv_reader]
             # print(iso_string[0])
             iso_string = str(iso_string[0]).split('-')
             print(iso_string)
 
 
 convert_date(2021-12-13)
+
+
 """Converts and ISO formatted date into a human readable format.
 
     Args:
@@ -62,25 +65,49 @@ pass
 
 
 def convert_f_to_c(temp_in_farenheit):
-    """Converts an temperature from farenheit to celcius.
+    with open("tests/data/example_one.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        next(csv_reader)
+        for temp in csv_reader:
+            min_celcius = (float(temp[1]) - 32) * 5.0/9.0
+            max_celcius = (float(temp[2]) - 32) * 5.0/9.0
+            print(
+                f"The minimum Celcius was {float(temp[1])}. The max Celcius was {float(temp[2])}.")
 
+
+convert_f_to_c(30)
+"""Converts an temperature from farenheit to celcius.
+(°F – 32) x 5/9 = °C
     Args:
         temp_in_farenheit: float representing a temperature.
     Returns:
         A float representing a temperature in degrees celcius, rounded to 1dp.
     """
-    pass
+pass
 
 
 def calculate_mean(weather_data):
-    """Calculates the mean value from a list of numbers.
+
+    with open("tests/data/example_one.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        next(csv_reader)
+        for row in csv_reader:
+            min_result = statistics.mean(int(row[1]))
+            max_result = statistics.mean(int(row[2]))
+            print(
+                f"The avarage min tempreture was {int(min_result)}. the avarage max_result was {int(max_result)}.")
+    return min_result, max_result
+
+
+calculate_mean(1)
+"""Calculates the mean value from a list of numbers.
 
     Args:
         weather_data: a list of numbers.
     Returns:
         A float representing the mean value.
     """
-    pass
+pass
 
 
 def load_data_from_csv(csv_file):
